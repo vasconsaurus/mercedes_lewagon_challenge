1587da5a6f833b7b69c7a249ecf8be7c96cfbfb0
manu vasconcelos <manu.vasconcelos@gmail.com>
2022-04-04 13:28:12 -0300

    edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  Rails.application.routes.draw do
-    resources :museums
-  end
+  # Rails.application.routes.draw do
+  #   resources :museums
+  # end
 
   get 'museums_results', to: 'museums#search', as: 'museums_results'
-  root to: 'museums#index'
+  # root to: 'museums#index'
 end
